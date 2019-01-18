33fede64d0965f8f727766d5668ccc9a0ac7fdb4
AlfiadoNgana <alfiadomaterialescolar@gmail.com>
2019-01-18 17:49:00 +0200

    Site esqueceu Senha

--- vendor/hcodebr/phpclasses/src/Model/User.php
+++ vendor/hcodebr/phpclasses/src/Model/User.php
@@ -132,7 +132,7 @@
             $sql->query("CALL sp_users_delete(:iduser)", array(":iduser"=>$this->getiduser()));
         }
 
-        public static function getForgot($email){
+        public static function getForgot($email, $inadmin=true){
 
             $sql = new Sql();
 
@@ -149,7 +149,11 @@
                     $dataRecovery = $result2[0];
 
                     $code=base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128,User::SECRET, $dataRecovery["idrecovery"], MCRYPT_MODE_ECB));
-                    $link="http://www.phpecomerce.co.mz/admin/forgot/reset?code=$code";
+                    if($inadmin){
+                        $link="http://www.phpecomerce.co.mz/admin/forgot/reset?code=$code";
+                    }else{
+                        $link="http://www.phpecomerce.co.mz/forgot/reset?code=$code";
+                    }
 
                     $mailer = new Mailer($data['desemail'], $data['desperson'], "Redefinir senha da Hcode Store","forgot",array(
                         "name"=>$data['desperson'],
